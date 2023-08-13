80e94b9426c3813c6c182b9f7cfd537b15f02bdf
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2023-08-12 21:52:02 -0300

    app.ts added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,5 @@
-import express from 'express';
-
-const app = express();
+import app from './app';
 
 app.listen(8000,() => {
     console.log('sever on port 8000')
 })
-
-console.log('hello world');
-const greeting: string = 'hello world';
-console.log('ya funca')
