c2d1526836ce3c0b6429690dd0646d2afe175b54
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2023-08-12 21:45:53 -0300

    backend configuration completed

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(8000,() => {
+    console.log('sever on port 8000')
+})
+
+console.log('hello world');
+const greeting: string = 'hello world';
+console.log('ya funca')
